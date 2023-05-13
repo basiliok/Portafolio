4f55ad4c265530cd930bda1e4388b9e3c8d656b2
Basilio <paledot01@gmail.com>
2023-05-13 00:54:57 -0500

    version 0.9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,25 @@ import { Observable } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit { // QUE MONGOL QUE SOY, ESTABA PROGRAMANDO FUERA DE LA CLASE.
 
   title = 'portafolio_kevin';
-  @ViewChild('modal_img') modalImagen: ElementRef; // <---------- llamamos al elemento del HTML
-  @ViewChild('caja_bolas') cajaBolas: ElementRef;
 
-  proy11: string[] = ["p11_img01.png", "p11_img02.png", "p11_img03.png"];
-  x: number = 0;
+  /** Bola: div exterior con forma esferica
+   *  Bolita: div initerior con forma esferica con relleno verde 
+   * */
+
+  @ViewChild('modal_img') modalImagen: ElementRef; // <---------- MODAL -> llamamos al elemento Imagen del HTML
+  @ViewChild('caja_bolas') cajaBolas: ElementRef; // <---------- MODAL -> Caja de Bolas
+
+  proy1: string[] = ["p1_img1.png", "p1_img2.png", "p1_img3.png"];
+  proy11: string[] = ["p11_img1.png", "p11_img2.png"];
+  
+  proy_imagenes: string[];
+  proy_numero: number;
+
+  x: number = 0; // --> indice para el orden de las bolitas
   arr_bolas: ElementRef[];
 
   constructor(private renderer: Renderer2){
@@ -23,18 +34,49 @@ export class AppComponent implements OnInit { // QUE MONGOL QUE SOY, ESTABA PROG
   }
   
   ngAfterViewInit(): void { // despues de que la vista se muestre
+  }
+
+
+  openModalGO(numero_proyecto: number):void{ // Identificacion del Proyecto, insercion de "Bolas" y de la primera imagen.
+    this.proy_numero = numero_proyecto; // identificamos el numero del proyecto
+    switch (numero_proyecto) { // obtenemos el arreglo de imagenes segun el numero del proyecto
+      case 1: 
+        this.proy_imagenes = this.proy1;
+        break;
+      case 11: this.proy_imagenes = this.proy11;
+        break;
+      default: this.proy_imagenes = this.proy11;
+        break;
+    }
+    
+    // insertamos el numero de "bolas" dependiendo del numero de imagenes que tiene el proyecto e insertamos la 1era "bolita" interior
     this.addBolas();
     this.changeBolita(0); // al inicio coloca la bolita en la 1era pocision.
+
+    // insertamos la primer imagen en el Modal dependiendo del proyecto elegido
+    const elemento = this.modalImagen.nativeElement; // obtenemos la etiqueta img
+    this.renderer.setAttribute(elemento, 'src', "assets/proyectos/proy_" + this.proy_numero +"/" + this.proy_imagenes[0]);
+  }
+
+  limpiar(){
+    this.x = 0;
+    // Eliminamos las "Bolas" de la "Caja de Bolas"
+    const div_contenedor = this.cajaBolas.nativeElement; // obtenemos el Papá
+    for (let index = 0; index < this.proy_imagenes.length; index++) {
+      const div_contenido = this.cajaBolas.nativeElement.children[0]; // obtenemos el Hijo (bola)
+      this.renderer.removeChild(div_contenedor, div_contenido);
+    }
   }
 
 
   addBolas(): void{ // AÑADIMOS LAS BOLITAS AL MODAL DEPENDIENDO DEL NUMERO DE IMAGENES
-    console.log('cantidad de imagenes es: ' + this.proy11.length);
-    for (let i = 0; i < this.proy11.length; i++) {
+    console.log('--------------------------')
+    console.log('Numero del Proyecto: ' + this.proy_numero);
+    console.log('Cantidad de Imagenes: ' + this.proy_imagenes.length)
+    for (let i = 0; i < this.proy_imagenes.length; i++) {
       const div = this.renderer.createElement('div'); // creamos un nuevo 'div'
       this.renderer.addClass(div,'bola'); // le añadimos su clase
       this.renderer.appendChild(this.cajaBolas.nativeElement, div); // le agregamos a la caja
-      console.log('i es: ' + i);
     }
   }
 
@@ -48,21 +90,17 @@ export class AppComponent implements OnInit { // QUE MONGOL QUE SOY, ESTABA PROG
 
 
   deleteBolitaPosterior(posicion: number): void{ // OBTENEMOS EL HIJO DEL HIJO PARA ELIMINARLO
-    if(posicion == 0){posicion = this.proy11.length}; // invertimos la condicion
+    if(posicion == 0){posicion = this.proy_imagenes.length}; // invertimos la condicion
     const div_bola_anterior = this.cajaBolas.nativeElement.children[posicion-1];
-    console.log(div_bola_anterior);
     const div_bolita_anterior = this.cajaBolas.nativeElement.children[posicion-1].children[0];
-    console.log(div_bolita_anterior);
     this.renderer.removeChild(div_bola_anterior, div_bolita_anterior);
   }
 
 
   deleteBolitaAnterior(posicion: number): void{ // OBTENEMOS EL HIJO DEL HIJO PARA ELIMINARLO
-    if(posicion == this.proy11.length - 1){posicion = -1}; // invertimos la condicion
+    if(posicion == this.proy_imagenes.length - 1){posicion = -1}; // invertimos la condicion
     const div_bola_anterior = this.cajaBolas.nativeElement.children[posicion+1];
-    console.log(div_bola_anterior);
     const div_bolita_anterior = this.cajaBolas.nativeElement.children[posicion+1].children[0];
-    console.log(div_bolita_anterior);
     this.renderer.removeChild(div_bola_anterior, div_bolita_anterior);
   }
 
@@ -70,9 +108,8 @@ export class AppComponent implements OnInit { // QUE MONGOL QUE SOY, ESTABA PROG
   adelante(): void{ // CAMBIAMOS LA IMAGEN POR LA QUE LE SIGUE
     this.x++; // aumentamos el contador
     const elemento = this.modalImagen.nativeElement; // obtenemos la etiqueta img
-    console.log(elemento);
-    if(this.x == this.proy11.length){this.x = 0};
-    this.renderer.setAttribute(elemento, 'src', "assets/proyectos/proy_01/" + this.proy11[this.x]); // modificamos su atributo
+    if(this.x == this.proy_imagenes.length){this.x = 0};
+    this.renderer.setAttribute(elemento, 'src', "assets/proyectos/proy_" + this.proy_numero +"/" + this.proy_imagenes[this.x]); // modificamos su atributo
     
     // cuando x llega al maximo vuelve a 0, change al 0 no hay problema, pero no se puede eliminar el (0-1)
     this.changeBolita(this.x);
@@ -83,9 +120,8 @@ export class AppComponent implements OnInit { // QUE MONGOL QUE SOY, ESTABA PROG
   atras(): void{ // CAMBIAMOS LA IMAGEN POR EL DE ATRAS
     this.x--; // disminuimos el contador
     const elemento = this.modalImagen.nativeElement; // obtenemos la etiqueta img
-    console.log(elemento);
-    if(this.x == -1){this.x = this.proy11.length - 1};
-    this.renderer.setAttribute(elemento, 'src', "assets/proyectos/proy_01/" + this.proy11[this.x]); // modificamos su atributo
+    if(this.x == -1){this.x = this.proy_imagenes.length - 1};
+    this.renderer.setAttribute(elemento, 'src', "assets/proyectos/proy_" + this.proy_numero + "/" + this.proy_imagenes[this.x]); // modificamos su atributo
 
     this.changeBolita(this.x); // coloca la bolita en la pocision x
     this.deleteBolitaAnterior(this.x); // elimina la bolita de la pocision 
