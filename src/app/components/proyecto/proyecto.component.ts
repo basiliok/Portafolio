711f457e64ee5ba6ce5588abd5f9065a0d78819f
basiliok <paledot01@gmail.com>
2025-09-18 17:20:34 -0500

    Add conditional background class for project component

--- src/app/components/proyecto/proyecto.component.ts
+++ src/app/components/proyecto/proyecto.component.ts
@@ -11,14 +11,14 @@ export class ProyectoComponent implements OnInit, AfterViewInit{
   @ViewChild('ePreview') elemento_preview: ElementRef;
   @ViewChild('eCode') elemento_code: ElementRef;
   @ViewChild('eOjo') elemento_ojo: ElementRef;
+  @ViewChild('eContainer') elemento_container: ElementRef;
 
   constructor(private renderer: Renderer2) { }
-  ngOnInit(): void {//throw new Error('Method not implemented.');
+  ngOnInit(): void {
   }
   ngAfterViewInit(): void {
-    this.changeAttribute();
-    //console.log("link: "+this.link_preview);
-    //throw new Error('Method not implemented.');
+    this.updatePreviewAttributes();
+    this.updateBackgroundColor();
   }
 
   @Input() number_proyect: number;
@@ -31,19 +31,25 @@ export class ProyectoComponent implements OnInit, AfterViewInit{
   @Input() link_github: string;
   @Input() duration: string;
   @Input() finish_date: string;
-  @Output() proyectoEvent = new EventEmitter<number>(); // output : 1
+  @Input() background_color: number;
+  @Output() proyectoEvent = new EventEmitter<number>(); // pasos output : 1
 
-  changeAttribute(): void{
+  updatePreviewAttributes(): void{
     if(this.link_preview === undefined){
       this.renderer.setAttribute(this.elemento_ojo.nativeElement, 'class', 'icon_muerto');
       this.renderer.removeAttribute(this.elemento_preview.nativeElement, 'href');
-      //this.renderer.removeAttribute(this.elemento_code.nativeElement, 'href');
     }
   }
 
-  emitirNumeroProy(): void{ // output : 2
-    this.proyectoEvent.emit(this.number_proyect);
-    //console.log("numero de proyecto: "+this.number_proyect);
+  updateBackgroundColor(): void {
+    if(this.background_color === 1){
+      this.renderer.addClass(this.elemento_container.nativeElement, 'bg-1');
+    } else if(this.background_color === 2){
+      this.renderer.addClass(this.elemento_container.nativeElement, 'bg-2');
+    }
   }
 
+  emitirNumeroDeProyectoParaAbrirModal(): void{ // pasos output : 2
+    this.proyectoEvent.emit(this.number_proyect);
+  }
 }
